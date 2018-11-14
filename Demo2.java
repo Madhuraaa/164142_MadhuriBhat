9440f1da0b1f9fc9057d4702bb6cb3a23000741b
Madhuraaa <44639051+Madhuraaa@users.noreply.github.com>
2018-11-14 20:39:04 +0530

    Add files via upload

---
+++ Demo2.java
@@ -0,0 +1,37 @@
+class Demo2
+{ public static void main(String[] args)
+{
+  int i;
+  for(i=1;i<=10;i++)
+{   if(i%2==0)
+System.out.println(i);
+
+}
+
+ int j;
+  for(j=1;j<=10;j++)
+  {   if(j%2!==0)
+System.out.println(j);
+
+   }
+
+
+  int k;
+  for(k=10;k>0;k--)
+ {
+ System.out.println(k);
+
+ }
+
+
+
+  int sum=0;
+  for(int l=1;l<=10;l++)
+  {
+    sum=sum+1;
+    System.out.println(sum);
+
+   }
+
+  
+   } }
