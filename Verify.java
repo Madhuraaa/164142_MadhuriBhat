7d726397d384f811cd94b1d53a547e68add377cc
Madhuraaa <44639051+Madhuraaa@users.noreply.github.com>
2018-11-15 18:00:20 +0530

    Add files via upload

---
+++ Verify.java
@@ -0,0 +1,41 @@
+package datas;
+
+import java.util.Scanner;
+
+public class Verify {
+	public void userValidation() {
+		Scanner sc = new Scanner(System.in);
+		String name, password;
+		String uname = "Madhuri";
+		String pword = "manasa";
+		int count = 0;
+		System.out.println("Enter your name");
+		name = sc.next();
+		System.out.println("Enter your password");
+		password = sc.next();
+		if ((name.equals(name)) && (password.equals(pword))) {
+			System.out.println("Welcome");
+		} else {
+			while (count < 2) {
+				if (!((name.equals(name)) && (password.equals(pword)))) {
+					System.out.println("Enter name");
+					name = sc.next();
+					System.out.println("Enter your password");
+					password = sc.next();
+				}
+				count = count + 1;
+			}
+
+		}
+		if (count == 2) {
+			System.out.println("Contact");
+		}
+
+	}
+
+	public static void main(String[] args) {
+		Verify character = new Verify();
+		character.userValidation();
+	}
+}
+
