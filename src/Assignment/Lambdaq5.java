905dd1cd6e5f94e1abf453d07606b7392b151eb0
Madhuraaa <44639051+Madhuraaa@users.noreply.github.com>
2018-11-30 09:21:22 +0530

    Update Lambdaq5.java

--- src/Assignment/Lambdaq5.java
+++ src/Assignment/Lambdaq5.java
@@ -2,4 +2,22 @@ package Assignment;
 
 public class Lambdaq5 {
 
+ List<String> words = new ArrayList<>();
+		  
+		  words.add("Patient");
+		  words.add("Admirable");
+		  words.add("Respected");
+		  words.add("Talented");
+		  words.add("Honest");
+		  words.add("Idealistic");
+		  
+		  System.out.println(words+"\n");
+		  	  
+		  words.stream()
+		  .forEach(w->System.out.println(w.replaceAll(w, w.toUpperCase())));
+		  
+		  
+		  
+	}
+
 }
