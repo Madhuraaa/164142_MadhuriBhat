538fb98c639bffedc27292272d8b1dc6c62c0684
Madhuraaa <44639051+Madhuraaa@users.noreply.github.com>
2018-11-30 11:48:17 +0530

    Update Lambdaq6.java

--- src/Assignment/Lambdaq6.java
+++ src/Assignment/Lambdaq6.java
@@ -7,8 +7,11 @@ public class Lambdaq6 {
 
 	public static void main(String[] args)
 	{
-		  List<String> words = new ArrayList<>();
-		  
+		 StringBuilder builder=new StringBuilder();
+		
+		  Consumer<List<String>> r = words->words.stream()
+				  .forEach(w->builder.append(w.charAt(0)));
+		  List<String> words=new ArrayList<>();
 		  words.add("Madhuri");
 		  words.add("Elite");
 		  words.add("Lavanya");
@@ -16,9 +19,8 @@ public class Lambdaq6 {
 		  words.add("Dayana");
 		  words.add("Yes");
 		  
-		  words.stream()
-		  .forEach(w->System.out.print(w.charAt(0)+"\t"));
-		 	
+		 r.accept(words);
+		 System.out.println(builder);
 	}
 
 }
