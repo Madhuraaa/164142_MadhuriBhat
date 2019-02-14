15384d3bc611b2a3292f7fbafe368da2f76ce9ca
Madhuraaa <44639051+Madhuraaa@users.noreply.github.com>
2019-02-14 15:31:45 +0530

    Add files via upload

---
+++ app/add-user.component.ts
@@ -0,0 +1,14 @@
+	import { Component } from '@angular/core';
+	
+	@Component({
+	  selector: 'add-user',
+	  template: `
+	    <h1>Add User</h1>
+	    <hr>
+   <p>TO DO ...</p>
+	  `
+	})
+export class AddUserComponent { 
+	itemCount: number = 4;
+
+}
